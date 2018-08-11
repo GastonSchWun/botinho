a4be79832f0a89f96835770cae19f999c0f228fa
Gaston Schattenhofer <gaston.schattenhofer@wunderman.com>
2018-08-10 23:24:49 -0300

    random logo quest; saving quest

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -5,6 +5,8 @@ const { getUser, setUser, getUserAndLogos } = require('../services/Firestore');
 /*const API_AI_TOKEN = '9d3388ad7f10465e95c1c8010517a270';
 const apiAiClient = require('apiai')(API_AI_TOKEN);*/
 
+let senderId2 = null;
+
 async function delay (senderId, ms) {
   await timeout(ms)
   let response = {
@@ -17,67 +19,73 @@ const timeout = (ms) => {
   new Promise(res => setTimeout(res, ms))
 };
 
-const getLogo = (logoId, logos) => {
-  if (logoId) {
-    return logos[logoId];
+const checkUngessedLogos = (logos, user) => {
+  let logosArray = [];
+  for (let logo in logos) {
+    if (logos.hasOwnProperty(logo)) {
+      if (!user || !user.quests || !user.quests.hasOwnProperty(logo) || !user.quests[logo]) {
+        logoArray.push(logo)
+      }
+    }
   }
 
-  let logosList = logos.all;
-  let logoRandom = logosList[Math.floor(Math.random()*logosList.length)];
-
-  return logos[logoRandom];
-};
-
-const getUnseenLogos = (allLogos, seenLogos) => {
-
+  return logosArray
 };
 
 const sendDefault = (senderId) => {
-  let response = { "text": "hola amigo" }
+  /*idealmente seria un boton*/
+  let response = { "text": "hola amigo! Cuando estes listo para adivinar escribime\n\"dame logo\"" }
   sendMessage(senderId, response);
 };
 
 const sendQuest = async (senderId) => {
   let { user, logos } = await getUserAndLogos(senderId);
-  let logo = getLogo(null, logos);
-  /*let quests = {}
+  let unguessedLogos = checkUngessedLogos(logos, user);
 
-  if (user && user.quests) {
+  if (unguessedLogos.length) {
+    let randomLogo = unguessedLogos[Math.floor(Math.random()*unguessedLogos.length)]
+    let userQuest = { quests:{}}
+    userQuests[randomLogo] = true
 
-  }*/
-
-  setUser(senderId, {}).then((res) => {
-    console.log(res)
-    let response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Este es el logo de " + logo.name +"?",
-            "subtitle": "Elegí tu respuesta.",
-            "image_url": logo.img,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Si!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
+    setUser(senderId, userQuest).then((res) => {
+      console.log('senderId2', senderId2)
+      let response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Este es el logo de " + logos[randomLogo].name +"?",
+              "subtitle": "Elegí tu respuesta.",
+              "image_url": logos[randomLogo].img,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Si!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
         }
       }
-    }
-    sendMessage(senderId, response)
-  });
+      sendMessage(senderId, response)
+    });
+    
+  } else {
+    let response = { "text": "ya adivinaste todos, sos un crack!" }
+    sendMessage(senderId, response);
+  }
+
 }
 
 module.exports = (senderId, message) => {
+  senderId2 = senderId
   if (message) {
     /*console.log("processMessage:: senderId", senderId)
     console.log("processMessage:: message", message)
